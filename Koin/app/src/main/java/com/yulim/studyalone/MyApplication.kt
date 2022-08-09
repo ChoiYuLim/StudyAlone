d460050c69b9ba19689c83993af65686a527d8f5
yulim <yulim13sky@naver.com>
2022-08-09 17:36:50 +0900

    [FEAT] androidLogger, androidContext 추가

--- Koin/app/src/main/java/com/yulim/studyalone/MyApplication.kt
+++ Koin/app/src/main/java/com/yulim/studyalone/MyApplication.kt
@@ -2,11 +2,15 @@ package com.yulim.studyalone
 
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 /*
     Application() 클래스를 상속받은 클래스에서 startKoin 메서드를 통해
     프로그램 내에 Kioin을 사용할 수 있도록 설정
+    androidLogger(): AndroidLogger를 Koin logger로 사용
+    androidContext(...): 해당 안드로이드 context를 사용
+    modules(...): 사용할 모듈을 등록
  */
 
 class MyApplication : Application() {
@@ -15,6 +19,8 @@ class MyApplication : Application() {
         super.onCreate()
 
         startKoin {
+            androidLogger()
+            androidContext(this@MyApplication)
             androidContext(applicationContext)
             modules(appModule)
         }
