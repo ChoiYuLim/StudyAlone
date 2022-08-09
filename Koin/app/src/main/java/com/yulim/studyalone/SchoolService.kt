8a9918dfb547a32cb64cc7beb0270509bc478e3c
yulim <yulim13sky@naver.com>
2022-08-09 17:18:16 +0900

    [FEAT] SchoolService

---
+++ Koin/app/src/main/java/com/yulim/studyalone/SchoolService.kt
@@ -0,0 +1,9 @@
+package com.yulim.studyalone
+
+class SchoolService {
+    var schoolName = "양서고등학교"
+
+    fun moveSchool(newSchoolName: String) {
+        this.schoolName = newSchoolName
+    }
+}
