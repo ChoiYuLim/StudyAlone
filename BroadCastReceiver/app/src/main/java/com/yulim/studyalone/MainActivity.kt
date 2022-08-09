5d08997c9e3930327064367221909be72d8a0dda
yulim <yulim13sky@naver.com>
2022-08-10 01:42:34 +0900

    [CHORE] 주석 추가 (안드로이드 8.0 이상은 정적 리시버 사용 불가)

--- BroadCastReceiver/app/src/main/java/com/yulim/studyalone/MainActivity.kt
+++ BroadCastReceiver/app/src/main/java/com/yulim/studyalone/MainActivity.kt
@@ -25,6 +25,13 @@ class MainActivity : AppCompatActivity() {
         -> 이유: 똑같이 br을 사용했기 때문, registerReceiver(br, filter)를 두번 호출했는데 unregisterReceiver(br)은 한번 호출했기 때문에 하나가 등록 해제가 안돼서.
 
         동적 리시버를 사용했기 때문에 onDestroy() 될 때까지 Receiver는 유효하지만, 여기서는 onPause() 함수 안에 unregisterReceiver를 했기 때문에 Receiver 해제함
+
+        *구글링한 결과 안드로이드 8.0부터는 정적 리시버를 사용할 수 없게 되었다. 정적 리시버가 아닌 동적 리시버로 구현해야한다.
+
+        Android 8.0
+        Beginning with Android 8.0 (API level 26), the system imposes additional restrictions on manifest-declared receivers.
+        If your app targets Android 8.0 or higher, you cannot use the manifest to declare a receiver for most implicit broadcasts (broadcasts that don't target your app specifically).
+        You can still use a context-registered receiver when the user is actively using your app.
         */
     }
 
